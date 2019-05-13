87faedcc550b317a78865b0e22f4f16b81a19c8a
Majid Mallis <mmallis87@gmail.com>
2019-05-13 13:39:28 -0700

    Pre-release of the 2.0.0 version
    ### Removed
    * configure_jwt_authorization_flow has been removed. Update to use either request_jwt_user_token or request_jwt_application_token
    * empty test placeholder files
    ### Changed
    * The SDK now supports version 19.1.02 of the DocuSign eSignature API.
    * SDK Release Version updated.
    * ApiException, ApiClient and Configuration classes have moved under client folder. New import statement was simplified. Example: from docusign_esign import ApiException
    * Using PyJWT and cryptography libraries for OAuth, instead of jwcrypto and py-oauth2
    ### Added
    * Added a new *tabGroupLabels* field to all Tabs models
    * Added a new *Witnesses* field to all Recipients models
    * Implemented models for Smart Sections feature
    * Implemented initial support of HMAC for DocuSign Connect
    ### Fixed
    * A bug with that could cause the *moveEnvelopes* method call to return a response without a *Content-Type* header. (DCM-2871)

---
+++ docusign_esign/client/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# import auth modules into client package
+from .auth.oauth import Account
+from .auth.oauth import Organization
+from .auth.oauth import Link
+from .auth.oauth import OAuth
+from .auth.oauth import OAuthToken
