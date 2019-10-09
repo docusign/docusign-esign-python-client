3f0c4eb49401ce3afdea380e06064b8c51768a98
Harsha Rahul Boggaram <harsharahul.boggaram@docusign.com>
2019-10-09 13:54:46 -0700

    GA Release 2.1.0
    GA 2.1.0 Release and updates to the changelog.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import os
 from setuptools import setup, find_packages, Command
 
 NAME = "docusign_esign"
-VERSION = "2.1.0rc1"
+VERSION = "2.1.0"
 # To install the library, run the following
 #
 # python setup.py install
