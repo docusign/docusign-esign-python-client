511922b87bf61990c4dc5c892e7d13c416d07df3
Harsha Rahul Boggaram <harsharahul.b.k@gmail.com>
2020-08-21 14:33:21 -0700

    Version v2.6.0 release (#85)
    * Version v2.6.0-v2-20.2.02.02 release
    
    * Update .travis.yml
    
    Co-authored-by: DevCenter-DocuSign <dcdev@docusign.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages, Command, os  # noqa: H301
 
 NAME = "docusign_esign"
-VERSION = "2.6.0rc1"
+VERSION = "2.6.0"
 # To install the library, run the following
 #
 # python setup.py install
