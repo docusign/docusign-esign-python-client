543168c47c29648f08ea63ffae54539da3729f0f
Harsha Rahul Boggaram <harsharahul.b.k@gmail.com>
2021-04-05 19:31:18 -0700

    v3.8.1rc3 (#78) (#103)
    Exposing public bulkEnvelopeApi
    
    Co-authored-by: Harsha Rahul Boggaram <HarshaRahul.Boggaram@docusign.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages, Command, os  # noqa: H301
 
 NAME = "docusign-esign"
-VERSION = "3.8.1rc2"
+VERSION = "3.8.1rc3"
 # To install the library, run the following
 #
 # python setup.py install
