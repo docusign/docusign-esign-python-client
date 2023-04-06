bd8504d6e4cdc35479b82a56931f6d81e5ece882
Ken Harris <kentheprogrammer@gmail.com>
2023-04-06 17:53:06 -0400

    Version 3.22.0-v2.1-23.1.01.01 release (#165)
    Co-authored-by: Ken Harris <Ken.Harris@docusign.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages, Command, os  # noqa: H301	
 
 NAME = "docusign-esign"
-VERSION = "3.21.0"
+VERSION = "3.22.0"
 # To install the library, run the following
 #
 # python setup.py install
