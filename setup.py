3981733e33097e4ce97b61a4c7e0a781be0b29ff
asif-docusign <165984682+asif-docusign@users.noreply.github.com>
2024-05-22 17:11:06 +0530

    Version 4.0.0-v2.1-24.1.01.00 release (#190)
    Co-authored-by: root <root@devcenteradmin.docusigntest.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages, Command, os  # noqa: H301	
 
 NAME = "docusign-esign"
-VERSION = "4.0.0rc1"
+VERSION = "4.0.0"
 # To install the library, run the following
 #
 # python setup.py install
